470590b5fdb0da0225d4960154addd05c981ef99
Holly Coloma <holly.life.15@gmail.com>
2024-01-27 14:15:25 +0000

    Add script to show menu on page load.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
-//Global varaibales
+//Global variables
+
+let menu = document.getElementById('menu-container').innerHTML;
+
+
+
+
+/*let style = document.createElement('style');
+style.innerHTML = `
+    menu {
+        color: yellow;
+        display: flex;
+        flex-direction: column;
+    }
+`;
+document.head.appendChild(style);  */
+
+//Setup styles to objects
+/*let styles = {
+    display: 'flex';
+    color: 'red';
+    flex-direction: 'column';
+}*/
+
+
+//Assign the styles to the elements
+//Object.assign(menu.style, styles);
+
+//FUNCTIONS
+
+window.onload = menuOptions();
+
+function menuOptions() {
+    document.getElementById("main-container").innerHTML = menu;
+    document.getElementById("mid-container").innerHTML;
+}
+
+/*helper function to add style to element
+function css(element, style) {
+    for (let property in style)
+        element.style[property] = style[property];
+}*/
+
