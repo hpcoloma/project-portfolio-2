c8efc8664fc1d55af5f9eb747517752927e91dd3
Holly Coloma <holly.life.15@gmail.com>
2024-01-29 21:03:30 +0000

    add event listener to show and hide info container, style to make container show on top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 //Global variables
 
 let menu = document.getElementById('menu-container').innerHTML;
+let libraryContainer = document.getElementById("library-container");
+
+
+
+
 let tabButtons = document.querySelectorAll('.tablinks');
-let i;
-
-
 
 //Iteration Statement
 
@@ -19,7 +21,9 @@ for (let i = 0; i < tabButtons.length; i++) {
 
     //Show the active tab
     document.getElementById(tabId).style.display = 'block';
-    });
+    document.getElementById(tabId).style.backgroundColor = 'rgb(86,191,181)';    
+});
+    
 }
 
 //FUNCTIONS
@@ -33,4 +37,60 @@ function menuOptions() {
    // document.getElementById("mid-container").innerHTML;
   
 }
+
+
+
+
+
+//To show/hide library container
+//Library container Show
+function showLibraryContainer() {
+    libraryContainer.style.display = 'block';
+}
+
+//Library container hide
+function hideLibraryContainer() {
+    libraryContainer.style.display = 'none';
+}
+
+//Information button elements
+let infoButton = document.getElementById("information-button");
+let infoButtonHide = document.getElementById("library-close");
+
+
+//Information event listeners
+infoButton.addEventListener('click', showLibraryContainer);
+infoButtonHide.addEventListener('click', hideLibraryContainer);
+
+
+
+//To show/hide instructions container
+//Instruction container show
+//Instruction container hide
+
+
+
+//To show calculate pay container occupying replacing all elements in the main-container
+
+
+//Basic calculation formula
+
+
+
+//Display result of basic calculation by inserting a table
+
+
+
+//Advanced calculation formula
+
+
+
+//Display result of advanced calculation by inserting a table
+
+
+
+
+
+
+
 
